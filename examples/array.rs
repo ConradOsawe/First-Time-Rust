626b73fb58bffa68cb2c971583277514ee63bc69
ConradOsawe <anthonyconrad93@gmail.com>
2024-11-09 16:55:50 +0530

    add tuples to array file

--- examples/array.rs
+++ examples/array.rs
@@ -1,4 +1,8 @@
 fn main () {
     let array = [1, 2, 3];
     println!("{} {} {}", array[0], array[1], array[2]);
+
+    let tuple = (true, 3, 4.5);
+    println!("{} {} {}", tuple.0, tuple.1, tuple.2);
 }
+
